9bde72797cba6750e18a32cdeb2f13a72d2802a2
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-03-18 22:33:47 +0545

    Initial upload

---
+++ Day2.py
@@ -0,0 +1,9 @@
+# Datatypes
+
+first_name = "Prashant"
+last_name = "Maharjan"
+age = 22
+address = "Basundhara"
+hobbies = ["Badmintion", "Gaming", "Playing Guitar"]
+
+print(f"My name is {first_name} {last_name}. \nMy age is: {age}.\nI live in {address}.\nMy hobbies are: {hobbies}")
---
+++ Day3.py
@@ -0,0 +1,31 @@
+# Indexing and Sclicing
+# String
+sen = "I am a ( programmer."
+
+# print(sen[::-1])
+
+# List
+my_list = ['a', 'b', 'c', 'd']
+# print(my_list)
+
+my_list[3] = "Donald"
+# print(my_list)
+
+# print(my_list[::2])
+
+# Make a list of your hobbies, print a sentence like "I like <list data>" using indexing
+my_hobbies = ['Playing Guitar', 'Cooking', 'Coding']
+
+for i in range(len(my_hobbies)):
+    print(f"I like {my_hobbies[i]}")
+
+
+for i in my_hobbies:
+    # print(f"I like { i }")
+
+    pass
+
+my_hobbies_tuple = ['Playing Guitar', 'Cooking', 'Coding']
+
+# print(my_hobbies_tuple[2])
+# print(my_hobbies_tuple[0:2])
