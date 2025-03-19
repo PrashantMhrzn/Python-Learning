f89480cb92e95a822d7a682523daa9c837c8eb22
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-03-19 15:29:14 +0545

    udpated day3 & learn typecasting on day4

--- Day3.py
+++ Day3.py
@@ -1,19 +1,22 @@
 # Indexing and Sclicing
 # String
+# [Starting_index: Ending_index: Step]
+
 sen = "I am a ( programmer."
 
-# print(sen[::-1])
+print(sen[::-1])
 
-# List
+# List indexing
 my_list = ['a', 'b', 'c', 'd']
-# print(my_list)
+print(my_list)
 
+# Changing values
 my_list[3] = "Donald"
-# print(my_list)
+print(my_list)
 
-# print(my_list[::2])
+print(my_list[::2])
 
-# Make a list of your hobbies, print a sentence like "I like <list data>" using indexing
+# Activity: Make a list of your hobbies, print a sentence like "I like <list data>" using indexing
 my_hobbies = ['Playing Guitar', 'Cooking', 'Coding']
 
 for i in range(len(my_hobbies)):
@@ -21,11 +24,17 @@ for i in range(len(my_hobbies)):
 
 
 for i in my_hobbies:
-    # print(f"I like { i }")
+    print(f"I like { i }")
 
     pass
 
 my_hobbies_tuple = ['Playing Guitar', 'Cooking', 'Coding']
 
-# print(my_hobbies_tuple[2])
-# print(my_hobbies_tuple[0:2])
+print(my_hobbies_tuple[2])
+print(my_hobbies_tuple[0:2])
+
+
+
+
+
+
