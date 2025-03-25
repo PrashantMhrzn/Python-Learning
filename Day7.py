b8b3601893f03ea6583b7da019007fa2599bf3f8
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-03-25 15:23:51 +0545

    Learned about methods for various data types

--- Day7.py
+++ Day7.py
@@ -5,3 +5,28 @@ value = 'endswithA'
 print(value.endswith('A'))
 print(value.find('s'))
 print(value.swapcase())
+
+# List Methods
+lst = [1, 1, 2, 3 ,4]
+
+lst.append('a')
+print(lst)
+
+times = lst.count(1)
+print(times)
+
+lst.insert(2, 'insert')
+print(lst)
+
+lst.pop(3)
+print(lst)
+
+# Set
+a = {1,2,3,4,5}
+b = {4,5,6,7,8}
+
+c = a.difference(b)
+print(c)
+
+a.difference_update(b)
+print(a)
