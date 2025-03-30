5a3579afb42e4e1e0010d086569d875a46a8d49c
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-03-30 18:22:20 +0545

    Added login file handling

--- Activity/login_register_filehand.py
+++ Activity/login_register_filehand.py
@@ -3,20 +3,23 @@
 # if login, get username, check if it exist in the file
 
 choice = input("Would you like to login or register?:\n1.Register\n2.Login\n> ")
-if choice == '1':
+if choice == '1': 
     username = input('Enter your username to be stored: ')
     login_file = open('D:/Dev/Python Learning/Activity/db.txt', 'a')
-    login_file.write(f'test')
-    login_file.write(f'\n{username}')
+    login_file.write(f',{username}')
     login_file.close()
 elif choice == '2':
     username = input('Enter your username: ')
     login_file = open('D:/Dev/Python Learning/Activity/db.txt', 'r')
     read = login_file.read()
-    if username in read:
-        print(f'Welcome {username}!')
-    else:
-        print('User doesn\'t exist')
+    users = read.split(',')
+    for user in users:
+        if user == username:
+            print(f'Welcome {username}!')
+            break
+        else:
+            print('User doesn\'t exist')
+            break
     login_file.close()
 else:
     print('Invalid input')
