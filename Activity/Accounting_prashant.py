d3d850945eccc84d519337c5881e5c35bf6dddd0
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-03-31 00:13:27 +0545

    Accounting acitivy added file handling

--- Activity/Accounting_prashant.py
+++ Activity/Accounting_prashant.py
@@ -10,63 +10,92 @@
 # is less than the balance, if the withdraw amount is less than the balance, subtract 
 # the withdraw amount from the balance, else print "insufficient balance"
 # if user is not valid, print "invalid user"
-
-user_data = {'ram': 'ram123', 'hari': 'hari123', 'u':'u'}
-account = {'ram': 10000, 'hari': 900000, 'u':890000}
-
-username = input(('Enter your username: '))
-password = input(('Enter your password: '))
-
-def show_balance(username):
-    return account.get(username)
-
-def add_balance(username, amt):
-    add_amt = account.get(username)
-    new_balance = add_amt + amt
-    return new_balance
-
-def wit_balance(username, amt):
-    initial_balance = account.get(username)
-    new_balance = initial_balance - amt
-    return new_balance
-
-# Check if the username is in database
-if username in user_data:
-    # Check if the passwords match
-    if user_data.get(username) == password:
-        print('LOGGED IN')
-        while True:
-            choice = input('''
-****Welcome to your account!***
-1. Check balance
-2. Add balance
-3. Withdraw balance
->
-''')        
-            # Catch error for wrong input to int values 
-            try:
+choice = input("Would you like to login or register?:\n1.Register\n2.Login\n> ")
+if choice == '1': 
+    username = input('Enter your username to be stored: ')
+    password = input('Enter your password to be stored: ')
+    bank_acc = open('D:/Dev/Python Learning/Activity/bank_acc.txt', 'a')
+    bank_acc.write(f'{username},{password},')
+    bank_acc.close()
+    initial_money = int(input('Enter money to be deposited: '))
+    money = open('D:/Dev/Python Learning/Activity/money.txt', 'a')
+    money.write(f'{initial_money},')
+    money.close()
+elif choice == '2':
+    inp_username = input('Enter your username: ')
+    inp_password = input('Enter your password: ')
+    bank_acc = open('D:/Dev/Python Learning/Activity/bank_acc.txt', 'r')
+    read = bank_acc.read()
+    users = read.split(',')
+    username = users[::2]
+    password = users[1::2]
+    credentials = dict(zip(username, password))
+    money = open('D:/Dev/Python Learning/Activity/money.txt', 'r')
+    read_money = money.read()
+    individual_money = read_money.split(',')
+    balance = dict(zip(username, individual_money))
+    print(balance)
+    print(balance.get(inp_username))
+    print(credentials)
+    if inp_username in credentials.keys():
+        if inp_password == credentials.get(inp_username):
+            print('LOGGED IN')
+            while True:
+                choice = input('''
+                ****Welcome to your account!***
+                1. Check balance
+                2. Add balance
+                3. Withdraw balance
+                >
+                ''')        
+                # Catch error for wrong input to int values 
+                # try:
                 if choice == '1':
                     # Calling relevant functions
-                    print(f'Your balance is: {show_balance(username)}')
+                    print(f'Your balance is: {balance.get(inp_username)}')
                 elif choice == '2':
                     add_amt = int(input('Enter your amount to add to your account: '))
-                    print(f'Your added new balance is: {add_balance(username, add_amt)}')
+                    new_amt = int(balance.get(inp_username)) + add_amt
+                    print(f'Your added new balance is: {new_amt}')
                 elif choice == '3':
                     wit_amt = int(input('Enter your amount to withdraw from your account: '))
                     # Checking if the amount entered can be withdrawn
-                    if account.get(username) >= wit_amt:
-                        print(f'Your new balance is: {wit_balance(username, wit_amt)}')
+                    if int(balance.get(inp_username)) >= wit_amt:
+                        deducted_amt = int(balance.get(inp_username)) - wit_amt
+                        print(f'Your new balance is: {deducted_amt}')
                     else:
                         print('Insufficient Balance!')
                 else:
                     print('Invalid input!')
-            except:
-                print('Enter number value to be added!')
+                # except:
+                #     print('Enter number value to be added!')
 
-            choice = input('Do you want to keep using the app?[y/n]: ').upper()
-            if choice != 'Y':
-                break
+                choice = input('Do you want to keep using the app?[y/n]: ').upper()
+                if choice != 'Y':
+                    break
+        else:
+            print('Password for the given username doesn\'t match! Enter a valid password!')
     else:
-        print('Password doesn\'t match to username!')
+        print('Username not found in database! Please enter a valid username!')
+    bank_acc.close()
 else:
-    print('Username doesn\'t exists')
+    print('Invalid input')
+
+# account = {'ram': 10000, 'hari': 900000, 'u':890000}
+
+
+# def show_balance(username):
+#     return account.get(username)
+
+# def add_balance(username, amt):
+#     add_amt = account.get(username)
+#     new_balance = add_amt + amt
+#     return new_balance
+
+# def wit_balance(username, amt):
+#     initial_balance = account.get(username)
+#     new_balance = initial_balance - amt
+#     return new_balance
+
+
+       
--- Day11.py
+++ Day11.py
@@ -1,12 +1,43 @@
 #File Handling
 
-read_file = open("D:/Dev/Python Learning/day11.txt", 'r')
-print(read_file.read())
+# read_file = open("D:/Dev/Python Learning/day11.txt", 'r')
+# print(read_file.read())
 
 
-write_file = open("D:/Dev/Python Learning/day11.txt", 'w')
-write_file.write("Completely new text")
+# write_file = open("D:/Dev/Python Learning/day11.txt", 'w')
+# write_file.write("Completely new text")
 
 
-app_file = open("D:/Dev/Python Learning/day11.txt", 'a')
-app_file.write(" Append to this file")
+# app_file = open("D:/Dev/Python Learning/day11.txt", 'a')
+# app_file.write(" Append to this file")
+
+choice = input("Would you like to login or register?:\n1.Register\n2.Login\n> ")
+if choice == '1': 
+    username = input('Enter your username to be stored: ')
+    password = input('Enter your password to be stored: ')
+    login_file = open('D:/Dev/Python Learning/Activity/bank_acc.txt', 'a')
+    login_file.write(f'{username},{password},')
+    login_file.close()
+elif choice == '2':
+    inp_username = input('Enter your username: ')
+    inp_password = input('Enter your password: ')
+    login_file = open('D:/Dev/Python Learning/Activity/bank_acc.txt', 'r')
+    read = login_file.read()
+    users = read.split(',')
+    username = users[::2]
+    password = users[1::2]
+    credentials = dict(zip(username, password))
+    print(users)
+    print(username)
+    print(password)
+    print(credentials)
+    if inp_username in credentials.keys():
+        if inp_password == credentials.get(inp_username):
+            print("Logged in")
+        else:
+            print('Password for the given username doesn\t match! Enter a valid password!')
+    else:
+        print('Username not found in database! Please enter a valid username!')
+    login_file.close()
+else:
+    print('Invalid input')
