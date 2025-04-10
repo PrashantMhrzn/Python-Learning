8fb507eb7f091e5c374a4daa73dcb0d6c3e7ca4c
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-04-10 15:32:37 +0545

    updated git ignore

--- Django Learning/base/urls.py
+++ Django Learning/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, contact_us, about_us
 
 urlpatterns = [
-    path('index/', index)
+    path('index/', index),
+    path('contactus/', contact_us),
+    path('about/', about_us),
 ]
--- Django Learning/base/views.py
+++ Django Learning/base/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hi')
+
+def contact_us(request):
+    return HttpResponse('Contact us here')
+
+def about_us(request):
+    return HttpResponse('Hi we are learning Django!')
 # Create your views here.
