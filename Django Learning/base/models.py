eccf9f200220c8ea8e1b15bfaf798c190d019d9d
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-04-10 15:26:30 +0545

    Django app setup

---
+++ Django Learning/base/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class TodoList(models.Model):
+    title = models.CharField(100)
+    description = models.TextField(100)
+    is_completed = models.BooleanField()
