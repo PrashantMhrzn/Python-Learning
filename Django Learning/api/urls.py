a89607e6cead9133d53adea7546a9f29ad01d308
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-05-04 23:53:13 +0545

    Added ModelViewSet for API ease

--- Django Learning/api/urls.py
+++ Django Learning/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('all', AllTodo.as_view()),
-    path('all/<id>', DetailTodo.as_view())
+    # path('all', AllTodo.as_view()),
+    # path('all/<id>', DetailTodo.as_view())
+    path('all/<pk>', TodoView.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update', 'patch': 'partial_update'})),
+    path('all', TodoView.as_view({'get': 'list', 'post': 'create'}))
+
 ]
--- Django Learning/api/views.py
+++ Django Learning/api/views.py
@@ -5,54 +5,59 @@ from .models import TodoList
 from .serializers import TodoListSerializer
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-class AllTodo(APIView):
+class TodoView(ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
 
-    def get(self, request):
-        all_data = TodoList.objects.all()
-        serializer = TodoListSerializer(all_data, many = True)
-        return Response(serializer.data)
+# class AllTodo(APIView):
+
+#     def get(self, request):
+#         all_data = TodoList.objects.all()
+#         serializer = TodoListSerializer(all_data, many = True)
+#         return Response(serializer.data)
     
-    def post(self, request):
-        serializer = TodoListSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({
-            "deatils": "New Todo Created"
-        })
+#     def post(self, request):
+#         serializer = TodoListSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({
+#             "deatils": "New Todo Created"
+#         })
 
 
-class DetailTodo(APIView):
+# class DetailTodo(APIView):
 
-    def get(self, request, id):
-        single_data = TodoList.objects.get(id = id)
-        serializer = TodoListSerializer(single_data)
-        return Response(serializer.data)
+#     def get(self, request, id):
+#         single_data = TodoList.objects.get(id = id)
+#         serializer = TodoListSerializer(single_data)
+#         return Response(serializer.data)
     
-    def delete(self, request, id):
-        single_data = TodoList.objects.get(id = id)
-        single_data.delete()
-        return Response({
-            "detail": "Your Todo has been Deleted!"
-        })
+#     def delete(self, request, id):
+#         single_data = TodoList.objects.get(id = id)
+#         single_data.delete()
+#         return Response({
+#             "detail": "Your Todo has been Deleted!"
+#         })
     
-    def put(self, request, id):
-        single_date = TodoList.objects.get(id = id)
-        serializer = TodoListSerializer(single_date, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({
-            "details": "Your Todo has been Updated!"
-        })
+#     def put(self, request, id):
+#         single_date = TodoList.objects.get(id = id)
+#         serializer = TodoListSerializer(single_date, data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({
+#             "details": "Your Todo has been Updated!"
+#         })
     
-    def patch(self, request, id):
-        single_data = TodoList.objects.get(id =id)
-        serializer = TodoListSerializer(single_data, data = request.data, partial = True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({
-            "details": "Your Todo list had been Patched!"
-        })
+#     def patch(self, request, id):
+#         single_data = TodoList.objects.get(id =id)
+#         serializer = TodoListSerializer(single_data, data = request.data, partial = True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({
+#             "details": "Your Todo list had been Patched!"
+#         })
     
 
 
